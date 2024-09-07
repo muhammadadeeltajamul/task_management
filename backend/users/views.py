b1f8c09bebbdbbad348c69228bd54af88252ea97
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-09-07 11:29:06 +0500

    chore: raise 400 if user already logged in

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.utils import get_response_object
@@ -7,6 +9,8 @@ from users.utils import get_response_object
 
 class LoginView(APIView):
     def post(self, request):
+        if request.user.is_authenticated:
+            return Response("User already logged in", status=status.HTTP_400_BAD_REQUEST)
         email = request.POST.get('email', '')
         password = request.POST.get('password', '')
         if not (email or password):
