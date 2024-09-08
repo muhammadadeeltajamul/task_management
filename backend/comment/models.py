1ee18dc1c6122aec9dd3f0d30b81cc1eccf0cb40
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-09-08 20:03:02 +0500

    feat: added comment model

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,14 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Comments(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, db_index=True)
+    description = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, db_index=True)
+    ticket = models.ForeignKey('ticket.Ticket', on_delete=models.CASCADE)
+    reactions = models.JSONField(default=list)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
