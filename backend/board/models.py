2ed46a05a214d4af84a4913fe3232dfa87114725
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-09-08 19:35:36 +0500

    feat: added board model

--- backend/board/models.py
+++ backend/board/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class Board(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, db_index=True)
+    name = models.CharField(max_length=512, db_index=True)
+    columns = models.JSONField(default=list)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, db_index=True)
+    private = models.BooleanField(default=True)
+
+    def add_column(self, column_name):
+        if column_name in self.columns:
+            raise ValidationError("Column name already exist")
+        self.columns.append(column_name)
+
+    def remove_column(self, column_name):
+        if column_name not in self.columns:
+            raise ValidationError("Column doesn't exist")
+        self.columns.remove(column_name)
