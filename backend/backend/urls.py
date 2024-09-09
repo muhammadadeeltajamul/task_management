d006491e45add7276b48ff70fc1a7838f7e21d39
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-09-09 08:35:38 +0500

    feat: added urls.py file in board app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import include,  path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('board/', include('board.urls')),
     path('user/', include('users.urls'))
 ]
