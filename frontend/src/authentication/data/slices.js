9e36aac986c0d5be183347db491589a9a2273981
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-10-12 21:27:03 +0500

    feat: added user slice

--- frontend/src/authentication/data/slices.js
+++ frontend/src/authentication/data/slices.js
@@ -1,15 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RequestStatus } from "../../constant";
 
+const defaultState = {
+  status: RequestStatus.INITIAL,
+  loggedIn: false,
+  email: '',
+  superuser: false,
+  errorMessage: '',
+};
+
 const userSlice = createSlice({
   name: 'user',
-  initialState: {
-    status: RequestStatus.INITIAL,
-    loggedIn: false,
-    email: '',
-    superuser: false,
-  },
-  reducers: {}
+  initialState: { ...defaultState },
+  reducers: {
+    loginUser: (state, { payload }) => ({
+      ...state,
+      status: RequestStatus.SUCCESSFUL,
+      loggedIn: true,
+      email: payload.email,
+      superuser: payload.superuser,
+    }),
+    logoutUser: (state, { payload }) => ({
+      ...defaultState,
+      status: RequestStatus.SUCCESSFUL,
+      loggedIn: false,
+      email: '',
+      superuser: false,
+    }),
+    loginFailed: (state, { payload }) => ({
+      ...state,
+      status: RequestStatus.FAILED,
+      errorMessage: payload.message,
+    }),
+    userDenied: (state, { payload }) => ({
+      ...state,
+      status: RequestStatus.DENIED,
+      errorMessage: payload.message,
+    })
+  }
 });
+
+export const {
+  loginUser, logoutUser, loginFailed, userDenied,
+} = userSlice.actions;
 
 export const userReducer = userSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { userReducer } from './authentication/data/slices';
 
 const initializeStore = (preloadedState = undefined) => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      user: userReducer,
+    },
     preloadedState,
   });
 }
