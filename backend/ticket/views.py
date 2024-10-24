5153a495ad83c14bc59556946cdc788114f8ddb9
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-10-24 10:53:06 +0500

    feat: added support for board_id param in ticket list api

--- backend/ticket/views.py
+++ backend/ticket/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from ticket.models import Ticket
@@ -8,4 +10,12 @@ class TicketViewSet(ModelViewSet):
     serializer_class = TicketSerializer
 
     def get_queryset(self):
+        board_id = self.request.GET.get('board_id')
+        if board_id:
+            return Ticket.objects.filter(board=board_id)
         return Ticket.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
