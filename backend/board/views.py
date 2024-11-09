b6e1ab6b80f9208116517e6694506981498b9eb5
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-11-09 22:06:11 +0500

    feat: override create method for board api

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,3 +1,5 @@
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 
 from board.models import Board
@@ -9,3 +11,16 @@ class BoardViewSet(ModelViewSet):
 
     def get_queryset(self):
         return Board.objects.filter(creator=self.request.user)
+
+    def create(self, request):
+        name = request.data.get("name")
+        if not name:
+            raise Exception("Name is required")
+        params = {
+            'name': name,
+            'description': request.data.get("description", ""),
+            'creator': request.user,
+        }
+        instance = Board.objects.create(**params)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
