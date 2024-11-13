b94f78c4cc11065a0e22c40e469a422420a45222
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-11-13 22:12:40 +0500

    feat: added patch for board api

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,9 +1,11 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 
 from board.models import Board
 from board.serializers import BoardSerializer
+from ticket.models import Ticket
 
 
 class BoardViewSet(ModelViewSet):
@@ -24,3 +26,15 @@ class BoardViewSet(ModelViewSet):
         instance = Board.objects.create(**params)
         serializer = self.get_serializer(instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, pk):
+        key = request.data.get("key")
+        value = request.data.get("value")
+        if not (key and value):
+            raise Exception("Need key and value to update")
+        if key not in ['name', 'description', 'columns', 'private']:
+            raise Exception(f"Invalid field {key}")
+        obj = get_object_or_404(Board, pk=pk)
+        setattr(obj, key, value)
+        obj.save()
+        return Response({key: value}, status=status.HTTP_200_OK)
