ce52e98e484271209ef3ffec60e432e13d63abea
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-11-19 18:08:54 +0500

    feat: created create method for ticket

--- backend/ticket/views.py
+++ backend/ticket/views.py
@@ -19,3 +19,24 @@ class TicketViewSet(ModelViewSet):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        title = request.data.get("title")
+        if not title:
+            raise Exception("Title is required")
+        board_id = request.data.get("board_id")
+        if not board_id:
+            raise Exception("board_id is required")
+        column_name = request.data.get("column_name")
+        if not column_name:
+            raise Exception("column_name is required")
+        params = {
+            'title': title,
+            'description': request.data.get("description", ""),
+            'board_id': board_id,
+            'column_name': column_name,
+            'creator': request.user,
+        }
+        instance = Ticket.objects.create(**params)
+        serializer = self.get_serializer(instance, context={'user': request.user})
+        return Response(serializer.data, status=status.HTTP_200_OK)
