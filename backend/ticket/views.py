31d4630f1b42bd1ef77e78893110c8b06df8cf7b
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-11-20 09:03:27 +0500

    feat: added patch request for ticket

--- backend/ticket/views.py
+++ backend/ticket/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from ticket.models import Ticket
 from ticket.serializers import TicketSerializer
+
+
+User = get_user_model()
 
 
 class TicketViewSet(ModelViewSet):
@@ -40,3 +45,19 @@ class TicketViewSet(ModelViewSet):
         instance = Ticket.objects.create(**params)
         serializer = self.get_serializer(instance, context={'user': request.user})
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, pk):
+        key = request.data.get("key")
+        value = request.data.get("value")
+        if key == "assigned_to":
+            obj = get_object_or_404(Ticket, pk=pk)
+            setattr(obj, key, User.objects.get(email=value) if value else None)
+        elif not (key and value):
+            raise Exception("Need key and value to update")
+        elif key not in ['title', 'description', 'column_name']:
+            raise Exception(f"Invalid field {key}")
+        else:
+            obj = get_object_or_404(Ticket, pk=pk)
+            setattr(obj, key, value)
+        obj.save()
+        return Response({key: value}, status=status.HTTP_200_OK)
