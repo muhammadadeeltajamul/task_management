73d8cc77397118cf01d5d47f5131cb0c3e6b9a50
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-11-23 10:13:41 +0500

    feat: added list comments API

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from comment.models import Comments
@@ -9,3 +11,16 @@ class CommentsViewSet(ModelViewSet):
 
     def get_queryset(self):
         return Comments.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        ticket_id = request.GET.get("ticket_id")
+        if not ticket_id:
+            raise ValidationError("Invalid ticket id")
+        queryset = self.get_queryset().filter(ticket_id=ticket_id)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True, context={'user': request.user})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True, context={'user': request.user})
+        return Response(serializer.data)
