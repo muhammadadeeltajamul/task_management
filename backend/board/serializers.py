c97278ee8530b5814438b05bbeb76963d0984de9
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-11-27 08:53:52 +0500

    feat: added permissions for board in serializer

--- backend/board/serializers.py
+++ backend/board/serializers.py
@@ -1,18 +1,28 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
+from board.board_access import get_allowed_actions
 from board.models import Board, BoardUsers
 
 
 class BoardSerializer(ModelSerializer):
     access_level = SerializerMethodField()
+    permissions = SerializerMethodField()
+
     class Meta:
         model = Board
         fields = '__all__'
     
     def get_access_level(self, obj):
         user = self.context.get('user')
-        board_user = BoardUsers.objects.get(board=obj, user=user)
+        if hasattr(self, '_cached_board_user'):
+            board_user = self._cached_board_user
+        else:
+            board_user = BoardUsers.objects.get(board=obj, user=user)
+            setattr(self, '_cached_board_user', board_user)
         return BoardUsersSerializer(board_user, many=False).data['access_level']
+
+    def get_permissions(self, obj):
+        return get_allowed_actions(self.get_access_level(obj))
 
 
 class BoardUsersSerializer(ModelSerializer):
