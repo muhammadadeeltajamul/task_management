1776736d7bbb706f5c65adc168cc99f265d50090
Muhammad Adeel Tajamul <muhammadadeeltajamul@hotmail.com>
2024-12-02 09:49:29 +0500

    chore: remove text from comment if successfully added

--- frontend/src/comments/CreateComment.jsx
+++ frontend/src/comments/CreateComment.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import AddIcon from '@mui/icons-material/Add';
 import { Avatar, TextField } from '@mui/material';
 import { selectBoard } from '../boards/data/selectors';
-import { Actions } from '../constant';
+import { Actions, RequestStatus } from '../constant';
+import { selectCommentsAPIStatus } from './data/selectors';
+import { setCommentsAPIStatus } from './data/slices';
 import { addNewComment } from './data/thunks';
 
 const CreateComment = ({ ticketId }) => {
   const dispatch = useDispatch();
   const { boardId } = useParams();
   const board = useSelector(selectBoard(boardId));
+  const statusName = 'createComment';
+  const createCommentStatus = useSelector(selectCommentsAPIStatus(statusName)).status;
   const [text, setText] = useState("");
+
+  useEffect(() => {
+    if (createCommentStatus === RequestStatus.SUCCESSFUL) {
+      setText("");
+      dispatch(setCommentsAPIStatus(RequestStatus.INITIAL));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [createCommentStatus])
 
   const onClickCreateComment = () => {
     const description = text.trim()
@@ -28,6 +40,7 @@ const CreateComment = ({ ticketId }) => {
       <TextField
         className="w-100"
         value={text}
+        disabled={createCommentStatus === RequestStatus.IN_PROGRESS}
         onChange={(e) => setText(e.target.value)}
         sx={{
           flexGrow: 1,
@@ -36,6 +49,7 @@ const CreateComment = ({ ticketId }) => {
       />
       <Avatar
         className="bg-transparent color-green my-auto mx-1r"
+        disabled={createCommentStatus === RequestStatus.IN_PROGRESS}
         style={{
           cursor: 'pointer',
           transition: 'transform 0.3s ease',
--- frontend/src/comments/data/selectors.js
+++ frontend/src/comments/data/selectors.js
@@ -1,1 +1,2 @@
 export const selectCommentsList = state => state.comments.comments;
+export const selectCommentsAPIStatus = apiName => state => state.comments.apiStatus[apiName];
